9c79140337e672ce4608b2cf8ee736d244dc242f
JackKit <chillkit@users.noreply.github.com>
2020-09-05 17:00:59 -0600

    Updates

---
+++ precache-manifest.ebe6991129dd06637c5dde15253ba157.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82eed3cbec731af0ef89302a54af8143",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57974ee6dc2eb711c8f0",
+    "url": "/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "4abebabfbc5c2cab36eb",
+    "url": "/static/js/2.e9e3d5a6.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.e9e3d5a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57974ee6dc2eb711c8f0",
+    "url": "/static/js/main.836324c4.chunk.js"
+  },
+  {
+    "revision": "910e8ae14cf021673fc1",
+    "url": "/static/js/runtime-main.59412213.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/chillkids/precache-manifest.9ae42193bc5749b0d164c5c41d32e0db.js"
+  "/precache-manifest.ebe6991129dd06637c5dde15253ba157.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/chillkids/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
