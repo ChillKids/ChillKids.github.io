592664a1dc879dda82351b93599cccc99c6adb29
JackKit <chillkit@users.noreply.github.com>
2021-11-07 22:35:02 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ebe6991129dd06637c5dde15253ba157.js"
+  "/precache-manifest.fd1907c4eed7da38caad9e8526a3f29f.js"
 );
 
 self.addEventListener('message', (event) => {
